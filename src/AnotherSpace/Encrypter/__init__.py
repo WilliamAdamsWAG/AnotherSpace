31868202189a6b1c8cbfa356f1ddb4b970a01099
William Adams <williamadams.aurora@gmail.com>
2024-12-29 00:41:24 +0300

    style: add docstring, annotations and comments

--- src/AnotherSpace/Encrypter/__init__.py
+++ src/AnotherSpace/Encrypter/__init__.py
@@ -1,24 +1,42 @@
 
 
-def caesar_cipher(text, shift):
-    encrypted_text = ""
-    
-    for char in text:
-        if char.isalpha():
-            position = ord(char)
+def caesar_cipher(text: str, shift: int) -> str:
+    """Encrypt a given text using the Caesar cipher algorithm.
 
-            new_position = position + shift
+    This function applies the Caesar cipher encryption method to a given text.
+    It shifts each alphabetic character by a specified number of positions while
+    leaving non-alphabetic characters unchanged.
+
+    Args:
+        `text (str)`: The input text to be encrypted.
+        `shift (int)`: The number of positions to shift each character by.
+
+    Returns:
+        str: The encrypted text.
+
+    Raises:
+        ValueError: If the shift value is not an `integer`.
+
+    Example:
+        >>> caesar_cipher("Hello, World!", 3)
+        'Khoor, Zruog!'
+    """
+
+    encrypted_text = ""
+
+    for char in text:
+        if char.isalpha():  # Check if the current character is alphabetic.
+            position = ord(char)  # Get the ASCII value of the character.
+
+            new_position = position + shift  # Calculate the new position after shifting.
 
             if char.isupper():
-                new_position = (new_position - ord('A')) % 26 + ord('A')
-            else:
-                new_position = (new_position - ord('a')) % 26 + ord('a')
+                new_position = (new_position - ord('A')) % 26 + ord('A')  # Adjust within A-Z range.
+            else: 
+                new_position = (new_position - ord('a')) % 26 + ord('a')  # Adjust within a-z range.
 
-            encrypted_text += chr(new_position)
+            encrypted_text += chr(new_position)  # Append the encrypted character to the result.
         else:
-
-            encrypted_text += char
+            encrypted_text += char  # Append non-alphabetic characters as they are.
 
     return encrypted_text
-
-
